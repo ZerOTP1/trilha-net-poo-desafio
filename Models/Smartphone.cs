10fa8eb7b7e2d6bd65a8eec43c063fb534128309
Marcos Tulio <marcostuliomatos1@gmail.com>
2025-01-02 01:30:46 -0300

    Att final

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,22 +3,37 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string Imei { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"Ligando {Modelo}");
         }
 
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+        }
+
+        public void VinculandoNumero()
+        {
+            Console.WriteLine($"Instalando Whatsapp no numero: {Numero}");
+        }
+        public void InformandoMemoria()
+        {
+            Console.WriteLine($"Memória disponível: {Memoria} GB");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero :"31 - 123456789", modelo: "Nokia Lumia 410", imei: "123456789", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+nokia.VinculandoNumero();
+nokia.InformandoMemoria();
+
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Ihpone");
+Smartphone iphone = new Iphone(numero :"21 - 975612461", modelo: "Iphone 11", imei: "987456321", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+iphone.VinculandoNumero();
+iphone.InformandoMemoria();
+iphone.ReceberLigacao();
